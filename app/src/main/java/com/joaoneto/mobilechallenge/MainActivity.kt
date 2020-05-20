5f7b4e1f156fbebe6e134f573912b2ee306306be
João <joao.ferreirawvn@gmail.com>
2020-05-20 09:58:31 -0300

    adding navigation view

--- app/src/main/java/com/joaoneto/mobilechallenge/MainActivity.kt
+++ app/src/main/java/com/joaoneto/mobilechallenge/MainActivity.kt
@@ -2,11 +2,33 @@ package com.joaoneto.mobilechallenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.navigation.NavigationView
+import java.util.function.ToDoubleBiFunction
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navigationView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpNavigationView()
+    }
+
+    private fun setUpNavigationView() {
+        navigationView.findViewById<NavigationView>(R.id.navigationView)
+        navigationView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_convertion -> {
+                    return@setNavigationItemSelectedListener true
+                }
+                R.id.navigation_list -> {
+
+                    return@setNavigationItemSelectedListener true
+                }
+
+            }
+            false
+        }
     }
 }
