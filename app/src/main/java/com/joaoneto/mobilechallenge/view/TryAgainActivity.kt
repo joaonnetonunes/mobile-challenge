c0d031523a590e24e6c900cfd6c2f74e08e9b304
João <joao.ferreirawvn@gmail.com>
2020-05-22 11:16:20 -0300

    adding api error treatment

--- app/src/main/java/com/joaoneto/mobilechallenge/view/TryAgainActivity.kt
+++ app/src/main/java/com/joaoneto/mobilechallenge/view/TryAgainActivity.kt
@@ -1,7 +1,9 @@
 package com.joaoneto.mobilechallenge.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.button.MaterialButton
 import com.joaoneto.mobilechallenge.R
 
 class TryAgainActivity : AppCompatActivity() {
@@ -9,5 +11,21 @@ class TryAgainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_try_again)
+        setUpButtons()
+
+    }
+
+    private fun setUpButtons(){
+        val buttonTryAgain = findViewById<MaterialButton>(R.id.buttonTryAgain)
+        val buttonExit = findViewById<MaterialButton>(R.id.buttonExit)
+
+        buttonTryAgain.setOnClickListener {
+            startActivity(Intent(this, SplashScreenActivity::class.java))
+            finish()
+        }
+
+        buttonExit.setOnClickListener {
+            finish()
+        }
     }
 }
